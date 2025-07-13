ef2c4c022820762c3b7a54f055227b2f13c1f2e8
Subrat090 <subratmahapatro09@gmail.com>
2025-07-13 19:01:04 +0530

    Initial commit

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = 'https://nhdakqivhwyvhidypnki.supabase.co'
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
